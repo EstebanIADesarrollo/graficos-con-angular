3670310f8e19b28944a055b6717b9e3d1af29370
EstebanIADesarrollo <esteban.avh.test@gmail.com>
2025-06-30 15:31:30 -0500

    Se implementa formulario con signals y formControl

--- src/app/components/formulario/formulario.ts
+++ src/app/components/formulario/formulario.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario.html',
   styleUrl: './formulario.css'
 })
 export class Formulario {
-
+  periodo = signal(new FormControl(''));
+  cuota = signal(new FormControl(''));
+  tasa = signal(new FormControl(''));
 }
